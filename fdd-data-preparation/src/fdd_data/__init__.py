0732301129eb3d2664957bdf10ed0157fd2abb29
rateshsachdeva <rateshsachdeva@gmail.com>
2026-08-19 23:01:06 +0530

    Use robust integrated data-preparation wrapper

--- fdd-data-preparation/src/fdd_data/__init__.py
+++ fdd-data-preparation/src/fdd_data/__init__.py
@@ -1,5 +1,6 @@
 """Embedded FDD data-preparation runtime used by the OCL skill."""
 
-from .integrated import DataPrepResult, prepare_source_package
+from .integrated import DataPrepResult
+from .robust import prepare_source_package
 
 __all__ = ["DataPrepResult", "prepare_source_package"]
